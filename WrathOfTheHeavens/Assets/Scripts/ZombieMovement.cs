9afef7b6c707d099ae496e72a9c9ee60758a7a4d
tkdejong <tim@raatdejong.nl>
2014-03-19 14:02:28 +0100

    Made zombies spawn and attack the town in TestScene.
    One zombie type, their speed and spawnInterval changes over time (10
    min), they disappear upon reaching the town.

---
+++ WrathOfTheHeavens/Assets/Scripts/ZombieMovement.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ZombieMovement : MonoBehaviour {
+	public float speed;
+	public float goalX;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate (new Vector2(speed * Time.deltaTime, 0f));
+		if (transform.position.x >= goalX) {
+			Debug.Log ("Zombie reached the town!");
+			Destroy(gameObject);
+		}
+	}
+}
+//""
---
+++ WrathOfTheHeavens/Assets/Scripts/ZombieScheduler.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ZombieScheduler : MonoBehaviour {
+	public AnimationCurve speed = AnimationCurve.Linear (0f, 0.3f, 600f, 5f);
+	public AnimationCurve spawnInterval = AnimationCurve.Linear (0f, 7f, 600f, .5f);
+
+	public float minSpawnY = -3.3f;
+	public float maxSpawnY = 3.3f;
+	public float spawnX = -6f;
+
+	public GameObject templateZombie;
+
+	float startTime;
+	float lastSpawn;
+
+	// Use this for initialization
+	void Start () {
+		startTime = Time.time;
+		lastSpawn = startTime;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//Debug.Log ("Speed = " + speed.Evaluate (Time.time - startTime) + " at time " + (Time.time - startTime));
+		if (Time.time - lastSpawn >= spawnInterval.Evaluate (lastSpawn)) {
+			lastSpawn = Time.time;
+			SpawnZombie(speed.Evaluate(Time.time));
+		}
+	}
+
+	//Spawns a zombie with a certain speed at a random y-pos on the left of the screen
+	void SpawnZombie(float speed) {
+		float y = Random.Range (minSpawnY, maxSpawnY);
+		Transform zombie = ((GameObject) Instantiate (templateZombie)).transform;
+
+		zombie.position = new Vector2 (spawnX, y);
+		zombie.GetComponent<ZombieMovement> ().speed = speed;
+	}
+}
