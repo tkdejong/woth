0af95e0326b506a877eb2f1574df96275b8f1fb6
tkdejong <tim@raatdejong.nl>
2014-03-19 14:59:30 +0100

    Added mockup crosshair.
    You can not yet kill zombies (it is implemented, but doesn't work - yet)

---
+++ WrathOfTheHeavens/Assets/Scripts/Attack.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class Attack : MonoBehaviour {
+	//The enemies that are currently colliding with the cursors collider
+	public List<Transform> underCursor;
+
+	// Use this for initialization
+	void Start () {
+		underCursor = new List<Transform>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown(0)) {
+			foreach (Transform zombie in underCursor)
+			{
+				//Send the zombie a message that it has been hit
+				zombie.SendMessage("Attacked");
+			}
+		}
+	}
+
+	//Keep track of what enemies are currently under the cursor 
+	void OnTriggerEnter(Collider other) {
+		if (other.GetComponent<Zombie> () != null) {
+			underCursor.Add (other.transform);
+		}
+	}
+	void OnTriggerExit(Collider other) {
+		underCursor.Remove (other.transform);
+	}
+}
---
+++ WrathOfTheHeavens/Assets/Scripts/MouseFollow.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseFollow : MonoBehaviour {
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 pos = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+		pos.z = 0f;
+		transform.position = pos;
+	}
+}
