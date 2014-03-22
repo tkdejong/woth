4148442867e1d36eedbfe1a4f651255350e226be
tkdejong <tim@raatdejong.nl>
2014-03-22 16:53:29 +0100

    Added particles to lightning strike.
    It's hard to get nice looking particles...

---
+++ WrathOfTheHeavens/Assets/Scripts/LightningParticles.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LightningParticles : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+	{
+		//Change Foreground to the layer you want it to display on 
+		//You could prob. make a public variable for this
+		particleSystem.renderer.sortingLayerName = "Lightning";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
