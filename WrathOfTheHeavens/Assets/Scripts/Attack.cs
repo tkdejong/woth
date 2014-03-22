10dbb1c2fcd71eae2e7dca5ef52f139df1698fb0
tkdejong <tim@raatdejong.nl>
2014-03-22 11:44:04 +0100

    Added mockup lightning: sprite and audio.
    Lightning sprite fades, lightning prefab selfdestructs.

--- WrathOfTheHeavens/Assets/Scripts/Attack.cs
+++ WrathOfTheHeavens/Assets/Scripts/Attack.cs
@@ -4,6 +4,8 @@ using System.Collections.Generic;
 public class Attack : MonoBehaviour {
 	//The enemies that are currently colliding with the cursors collider
 	List<Transform> underCursor;
+	//The lightning bolt that is spawned on attack
+	public GameObject lightningBolt;
 
 	// Use this for initialization
 	void Start () {
@@ -12,7 +14,14 @@ public class Attack : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		//Check if attack is executed
 		if (Input.GetMouseButtonDown(0)) {
+			//Display the effects of the attack on the targeted location
+			Vector3 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+			pos.z = 0;
+			Transform lightning = ((GameObject) Instantiate (lightningBolt)).transform;
+			lightning.position = pos;
+
 			//Keep track of the zombies that should be removed,
 			//  without modifying the list in mid-loop
 			List<Transform> killedZombies = new List<Transform>();
---
+++ WrathOfTheHeavens/Assets/Scripts/Lightning.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Lightning : MonoBehaviour {
+	//Time over which the sprite goes from opaque to completely transparent (in seconds)
+	public float fadeTime;
+	//How long before this object removes itself
+	public float lifeTime;
+
+	SpriteRenderer sprite;
+	//Time when the object is created
+	float creationTime;
+
+	// Use this for initialization
+	void Start () {
+		sprite = GetComponent<SpriteRenderer>();
+		creationTime = Time.time;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//Slowly decrease the sprites alpha channel (transparency) from 1 to 0
+		Color color = sprite.color;
+		color.a -= Time.deltaTime/fadeTime;
+		sprite.color = color;
+
+		//See if it is time to self-destruct
+		if (Time.time > creationTime + lifeTime) {
+			Destroy (gameObject);
+		}
+	}
+}
