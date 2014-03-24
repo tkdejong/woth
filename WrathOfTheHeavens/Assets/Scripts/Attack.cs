c3cae6a904054f9e1150dab0ef5d760e7853644d
Nicola Chinellato <nicola.chinellato.42@gmail.com>
2014-03-24 16:07:08 +0100

    Added cooldown to lightning

--- WrathOfTheHeavens/Assets/Scripts/Attack.cs
+++ WrathOfTheHeavens/Assets/Scripts/Attack.cs
@@ -7,35 +7,49 @@ public class Attack : MonoBehaviour {
 	//The lightning bolt that is spawned on attack
 	public GameObject lightningBolt;
 
+	private float cooldown;
+	private static float maxCooldown = 2;
+
 	// Use this for initialization
 	void Start () {
 		underCursor = new List<Transform>();
+		cooldown = maxCooldown;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		//Check if attack is executed
-		if (Input.GetMouseButtonDown(0)) {
-			//Display the effects of the attack on the targeted location
-			Vector3 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-			pos.z = 0;
-			Transform lightning = ((GameObject) Instantiate (lightningBolt)).transform;
-			lightning.position = pos;
 
-			//Keep track of the zombies that should be removed,
-			//  without modifying the list in mid-loop
-			List<Transform> killedZombies = new List<Transform>();
+		//Check if attack can be executed
+		if(cooldown >= maxCooldown) {
 
-			foreach (Transform zombie in underCursor)
-			{
-				//Send the zombie a message that it has been hit
-				zombie.SendMessage("Attacked");
-				killedZombies.Add(zombie);
+			//Check if attack is executed
+			if (Input.GetMouseButtonDown(0)) {
+				//Display the effects of the attack on the targeted location
+				Vector3 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+				pos.z = 0;
+				Transform lightning = ((GameObject) Instantiate (lightningBolt)).transform;
+				lightning.position = pos;
+
+				//Keep track of the zombies that should be removed,
+				//  without modifying the list in mid-loop
+				List<Transform> killedZombies = new List<Transform>();
+
+				foreach (Transform zombie in underCursor)
+				{
+					//Send the zombie a message that it has been hit
+					zombie.SendMessage("Attacked");
+					killedZombies.Add(zombie);
+				}
+				foreach (Transform killed in killedZombies)
+				{
+					underCursor.Remove (killed);
+				}
+
+				cooldown = 0;
 			}
-			foreach (Transform killed in killedZombies)
-			{
-				underCursor.Remove (killed);
-			}
+		}
+		else {
+			cooldown += Time.deltaTime;
 		}
 	}
 
