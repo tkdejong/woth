2896c5cb8ca030d2d56e0352b5ab7aa300e84b70
tkdejong <tim@raatdejong.nl>
2014-03-26 12:41:24 +0100

    Merijn: added different zombie types

--- WrathOfTheHeavens/Assets/Scripts/ZombieScheduler.cs
+++ WrathOfTheHeavens/Assets/Scripts/ZombieScheduler.cs
@@ -2,40 +2,57 @@
 using System.Collections;
 
 public class ZombieScheduler : MonoBehaviour {
-	public AnimationCurve speed = AnimationCurve.Linear (0f, 0.3f, 600f, 5f);
-	public AnimationCurve spawnInterval = AnimationCurve.Linear (0f, 7f, 600f, .5f);
-
+	public AnimationCurve spawnInterval1 = AnimationCurve.Linear (0f, 7f, 600f, .5f);
+	public AnimationCurve spawnInterval2 = AnimationCurve.Linear (0f, 14f, 600f, 1.0f);
+	public AnimationCurve spawnInterval3 = AnimationCurve.Linear (0f, 21f, 600f, 1.5f);
+	
 	public float minSpawnY = -3.3f;
 	public float maxSpawnY = 3.3f;
 	public float spawnX = -6f;
-
+	
 	public GameObject templateZombie;
-
+	
 	float startTime;
-	float lastSpawn;
-
+	float lastSpawn1, lastSpawn2, lastSpawn3;
+	
 	// Use this for initialization
 	void Start () {
 		startTime = Time.time;
-		lastSpawn = startTime;
+		lastSpawn1 = startTime;
+		lastSpawn2 = startTime;
+		lastSpawn3 = startTime;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		//Debug.Log ("Speed = " + speed.Evaluate (Time.time - startTime) + " at time " + (Time.time - startTime));
-		if (Time.time - lastSpawn >= spawnInterval.Evaluate (lastSpawn)) {
-			lastSpawn = Time.time;
-			SpawnZombie(speed.Evaluate(Time.time));
+		
+		//Zombie Type 1
+		if (Time.time - lastSpawn1 >= spawnInterval1.Evaluate (lastSpawn1)) {
+			lastSpawn1 = Time.time;
+			SpawnZombie(0.3f);
+		}
+		
+		//Zombie Type 2
+		if (Time.time - lastSpawn2 >= spawnInterval2.Evaluate (lastSpawn2)) {
+			lastSpawn2 = Time.time;
+			SpawnZombie(2.0f);
+		}
+		
+		//Zombie Type 1
+		if (Time.time - lastSpawn3 >= spawnInterval3.Evaluate (lastSpawn3)) {
+			lastSpawn3 = Time.time;
+			SpawnZombie(4.0f);
 		}
 	}
-
+	
 	//Spawns a zombie with a certain speed at a random y-pos on the left of the screen
 	void SpawnZombie(float speed) {
 		float y = Random.Range (minSpawnY, maxSpawnY);
 		Transform zombie = ((GameObject) Instantiate (templateZombie)).transform;
-
+		
 		zombie.position = new Vector2 (spawnX, y);
 		zombie.GetComponent<ZombieMovement> ().speed = speed;
 	}
-
+	
 }
