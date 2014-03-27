39bfed6d6f5612c78f0ad3b3c59da2bed0e87f1b
tkdejong <tim@raatdejong.nl>
2014-03-27 12:23:16 +0100

    Fitted GUI, added 50% wall, fixed some numbers.
    Fixed numbers: where zombies can spawn and how far they have to walk. TO
    TEST: Does the GUI look good on laptop-size screen?

--- WrathOfTheHeavens/Assets/Scripts/GUI/DrawText.cs
+++ WrathOfTheHeavens/Assets/Scripts/GUI/DrawText.cs
@@ -6,16 +6,16 @@ public class DrawText : MonoBehaviour {
 	//Position of the text in relative coordinates from the sprite(s top left corner)
 	public float textX = 1.3f;
 	public float textY = -0.25f;
+	//Bottom right corner of the label in which the text is displayed, in screen pixels
+	public float textRight = 0f;
+	public float textBottom = 0f;
 
 	//Text style settings
 	public int fontSize = 12;
 	public bool bold;
 	public Color textColor = new Color(0f, 0f, 0f, 1f);
 
-	//Dimensions of the label in which the text is displayed, in screen pixels
-	// .. these may not be very important ..
-	public float textScreenWidth = 150f;
-	public float textScreenHeight = 80f;
+
 
 	//The color of the bar
 	//public Color barColor = new Color(1,0,0,1f);
@@ -24,6 +24,7 @@ public class DrawText : MonoBehaviour {
 	//If the position is static, this is calculated only once, instead of every frame.
 	public bool staticPosition;
 	private Vector3 textPos;
+	private Vector3 bottomRight;
 
 	private GUIStyle style;
 	
@@ -37,7 +38,7 @@ public class DrawText : MonoBehaviour {
 	private void setStyle() {
 		if (style == null) {
 			style = new GUIStyle( GUI.skin.label );
-			style.clipping = TextClipping.Overflow;
+			style.clipping = TextClipping.Clip;
 		
 			style.fontSize = fontSize;
 			if (bold) {
@@ -51,12 +52,15 @@ public class DrawText : MonoBehaviour {
 	//Calculates the on-screen drawing coordinates for the bar,
 	//  from the current transform of the sprite and the given relative bounds (l/r/t/b).
 	private void CalculateCoordinates() {
-		//Calculate the extents of the bar in world coordinates
+		//Calculate the extents of the text in world coordinates
 		Vector3 textPosWorld = transform.TransformPoint (new Vector3 (textX, textY, 0f));
+		Vector3 bottomRightWorld = transform.TransformPoint (new Vector3 (textRight, textBottom, 0f));
 		
 		//Change to screen coordinates as required for GUI drawing
 		textPos = Camera.main.WorldToScreenPoint(textPosWorld);
 		textPos.y = Screen.height - textPos.y;
+		bottomRight = Camera.main.WorldToScreenPoint(bottomRightWorld);
+		bottomRight.y = Screen.height - bottomRight.y;
 	}
 	
 	//Draws the bar on screen, fitting it in the sprite and scaling it
@@ -69,7 +73,7 @@ public class DrawText : MonoBehaviour {
 
 		//Draw the text on the calculated position
 		GUI.depth = -1;
-		GUI.Label (new Rect(textPos.x, textPos.y, textScreenWidth, textScreenHeight), GetText (), style);
+		GUI.Label (new Rect(textPos.x, textPos.y, bottomRight.x - textPos.x, bottomRight.y - textPos.y), GetText (), style);
 	}
 	
 	//Subclasses can decide what the text should be
